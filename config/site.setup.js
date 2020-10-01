421c02dda858c9901e60da3b1cbc685ce389144b
Yuichi Aoki <sena.rn68@gmail.com>
2020-10-01 18:38:18 +0900

    remove jQuery option.

--- config/site.setup.js
+++ config/site.setup.js
@@ -53,12 +53,6 @@ async function runSetup() {
       name: 'cssreset',
       message: 'Which CSS reset library would you like installed?',
       choices: ['normalize.css', 'reset.css', 'sanitize.css', 'None'],
-    },
-    {
-      type: 'select',
-      name: 'jquery',
-      message: 'Would you like jQuery installed?',
-      choices: ['Yes', 'No'],
     }
   ]);
 
@@ -87,17 +81,6 @@ async function runSetup() {
       + '@import "~' + questions.cssreset + '"\n';
 
     fs.writeFile(path.join(ROOT, '/src/stylesheets/styles.scss'), cssContent, (err) => {});
-  }
-
-  // Add jQuery to scripts
-  if (questions.jquery == 'Yes') {
-    const jsContent =
-      '// Load jQuery from NPM\n'
-      + 'import $ from \'jquery\';\n\n'
-      + 'window.jQuery = $;\n'
-      + 'window.$ = $;\n';
-
-    fs.writeFile(path.join(ROOT, '/src/javascripts/scripts.js'), jsContent, (err) => {});
   }
 };
 
